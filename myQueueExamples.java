11f5778d80f57171cfa3aefc2d9b06768da48c3f
ahmadadams97 <36199584+ahmadadams97@users.noreply.github.com>
2018-03-20 10:42:09 -0400

    Add files via upload

---
+++ myQueueExamples.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Student
+ *
+ */
+public class myQueueExamples {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ myStackexample.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+/**
+ * @author Student
+ *
+ */
+public class myStackexample {
+
+}
